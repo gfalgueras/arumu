ae6bf9b8b555b3c381a5dc71f7938dabad81eace
gfalgueras <ginardfalgueras@gmail.com>
2026-05-15 19:54:09 +0200

    Changes

--- src/main/index.ts
+++ src/main/index.ts
@@ -180,6 +180,7 @@ function createWindow() {
     width: 1280,
     height: 800,
     autoHideMenuBar: true,
+    icon: path.join(__dirname, '../../resources/icon.png'),
     webPreferences: {
       preload: path.join(__dirname, '../preload/index.js'),
       contextIsolation: true,
