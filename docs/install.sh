7dbe86fbf8e3d7d7ad50921dbc9e89ca4984da1e
gfalgueras <ginardfalgueras@gmail.com>
2026-05-15 19:59:25 +0200

    chore: remove test docs/ artifacts

--- docs/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-[[ $EUID -ne 0 ]] && { echo "Run as root: sudo bash"; exit 1; }
-curl -fsSL https://gfalgueras.github.io/arumu/arumu.repo \
-  -o /etc/yum.repos.d/arumu.repo
-dnf install -y arumu
-echo "Arumu installed."
